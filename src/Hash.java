361815cb8ad24c3703b826d71f0d6bdb7df9517e
Faisal S.A <Ksa45560@gmail.com>
2021-11-11 15:19:45 +0300

    Classes needed (maybe)

---
+++ src/Hash.java
@@ -0,0 +1,10 @@
+import java.security.MessageDigest;
+
+public class Hash {
+    public static void main(String[] args) {
+        // For test only
+        // for example
+        Hash hash = new Hash();
+        System.out.println(hash.hash("ملف تبي تشوف تشفر بشكل صحيح او لا"));
+    }
+}
