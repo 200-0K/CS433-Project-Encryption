f63ad9529a8ed337683f41c8dc1141fdbf323be7
suliman <sulimanalazizi@gmail.com>
2021-12-05 19:54:45 +0300

    Done

--- src/menus/HashMenu.java
+++ src/menus/HashMenu.java
@@ -14,31 +14,44 @@ import utils.FileSystem;
 import utils.UserInput;
 
 public class HashMenu implements IMenu {
-    
+
     private Hash hash = new Hash();
     private FileSystem fileSystem;
     private Scanner sc;
 
     public void run() {
         sc = new Scanner(System.in);
+        getAlgorithm();
+        processFiles();
+    }
+
+    public void getAlgorithm() {
         while (true) {
             ALGORITHM[] algorithms = ALGORITHM.values();
 
             System.out.println("Choose an Algorithms:");
-            for (int i = 0; i < algorithms.length; i++) System.out.println( (i+1) + ". " + algorithms[i].getAlgorithm() );
-            System.out.println( (algorithms.length + 1) + ". Back to Main Menu");
+            for (int i = 0; i < algorithms.length; i++)
+                System.out.println((i + 1) + ". " + algorithms[i].getAlgorithm());
+            System.out.println((algorithms.length + 1) + ". Back to Main Menu");
             System.out.print("Enter your choice: ");
 
             int algorithmNum = UserInput.getNumberFromUser();
             try {
-                hash.setAlgorithm(algorithms[algorithmNum-1]);
+                hash.setAlgorithm(algorithms[algorithmNum - 1]);
             } catch (IndexOutOfBoundsException e) {
-                if (algorithmNum == algorithms.length + 1) return;
+                if (algorithmNum == algorithms.length + 1)
+                    return;
                 System.out.println("Please choose a number from the list");
             } catch (Exception e) {
-                System.out.println("Error: "+e.getMessage());
+                System.out.println("Error: " + e.getMessage());
             }
 
+            processFiles();
+        }
+    }
+
+    public void processFiles() {
+        while (true) {
             this.fileSystem = UserInput.getFileSystemFromUser();
             FileSystem[] files = getFiles();
 
@@ -47,18 +60,18 @@ public class HashMenu implements IMenu {
                     file.read((fileBytes, isFinal) -> {
                         hash.update(fileBytes);
 
-                        if(isFinal) {
-                            System.out.println(file.getFile().getName() + ": " + new BigInteger(1,  hash.digest()).toString(16));
+                        if (isFinal) {
+                            System.out.println(
+                                    file.getFile().getName() + ": " + new BigInteger(1, hash.digest()).toString(16));
                         }
-                        
+
                     });
                 } catch (IOException e) {
                     System.out.println("I/O Error");
                     e.printStackTrace();
                 }
-            } 
-        } 
-
+            }
+        }
     }
 
     private FileSystem[] getFiles() {
@@ -66,17 +79,5 @@ public class HashMenu implements IMenu {
         if (files == null)
             files = new FileSystem[] { fileSystem };
         return files;
-
-        // Loop:
-            // ask about type of algorithm or go back to main menu
-            // ask about file/dir path
-            
-            // Loop: process files one by one (Copy getFiles() from EncryptMenu)
-                // call update for the message digest object with the help of FileSystem.read callback (See processFiles() method from EncryptMenu)
-                // use isFinal value of the callback to call digest method
-                // print digest value using BigInteger(1, digest).toString(16), e.g.
-                    // test.txt: ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff
-            //
-        //
     }
 }
