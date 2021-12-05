7e418e6498bd1671765b915bb2781918f0772ce9
Faisal S.A <Ksa45560@gmail.com>
2021-12-05 21:18:01 +0300

    getNumber with range

--- src/utils/UserInput.java
+++ src/utils/UserInput.java
@@ -5,20 +5,30 @@ import java.util.Scanner;
 
 public class UserInput {
     public static int getNumberFromUser() {
+        return getNumberFromUser(null, null);
+    }
+
+    public static int getNumberFromUser(Integer min, Integer max) {
+        boolean withRange = (min != null && max != null);
         Scanner sc = new Scanner(System.in);
         int num;
         while (true) {
             try {
                 num = sc.nextInt();
+
+                if (withRange) {
+                    if (num < min || num > max) throw new InputMismatchException();
+                }
+
                 break;
             } catch (InputMismatchException e) {
-                System.out.println("You must enter a number, try again");
-                sc.nextLine(); // important to clear the buffer
+                if (withRange) System.out.println("You must enter a number between ("+min+"-"+max+"), try again");
+                else System.out.println("You must enter a number, try again");
             } catch (Exception e) {
                 System.out.println("Error "+e.getMessage()+", try again");
                 e.printStackTrace();
-                sc.nextLine(); // important to clear the buffer
             }
+            sc.nextLine(); // important to clear the buffer
             System.out.print(": ");
         }
         return num;
