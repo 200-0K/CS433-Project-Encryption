28590145ae3eae73c4bd7bec81bce7076c44b7e4
Faisal S.A <Ksa45560@gmail.com>
2022-06-03 19:44:21 +0300

    *pause for build.bat

--- build.bat
+++ build.bat
@@ -4,6 +4,6 @@ cd src
 cd ..\out
 mkdir jar
 "C:\Program Files\Java\jdk-11.0.1\bin\jar.exe" cvfm jar\Encryption.jar ..\src\MANIFEST.MF .
-::cls
-::"C:\Program Files\Java\jdk-11.0.1\bin\java.exe" -jar jar\Encryption.jar
+cls
+"C:\Program Files\Java\jdk-11.0.1\bin\java.exe" -jar jar\Encryption.jar
 pause
